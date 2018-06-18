e5fd132e485b990693ae756484005a5085b6d0d0
andrew <uandrew1965@gmail.com>
2018-06-18 15:01:34 +0300

    .

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
+from django.urls import resolve
 
 
-class SmokeTest(TestCase):
-    def test_bad(self):
-        self.assertEqual(1 + 1, 3)
+# class SmokeTest(TestCase):
+#     def test_bad(self):
+#         self.assertEqual(1 + 1, 3)
+
+
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_root_url(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
