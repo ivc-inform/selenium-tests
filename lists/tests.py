13b8368e2f4b29d360111ba312749872e1f2260a
andrew <uandrew1965@gmail.com>
2018-06-23 11:23:53 +0300

    .

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
-
 
 # class SmokeTest(TestCase):
 #     def test_bad(self):
@@ -12,5 +12,13 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
     def test_root_url(self):
-        found = resolve("/")
+        found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf-8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn('<title>To-Do list</title>', html)
+        self.assertTrue(html.endswith("</html>"))
--- project/urls.py
+++ project/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import url
+from django.contrib.auth import admin
 from django.urls import path
 
 from lists.views import home_page
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', home_page, "home"),
+    path('', home_page, name = "home"),
 ]
