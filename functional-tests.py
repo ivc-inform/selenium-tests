96c04f633a1e319e77ab9f643d4b849ae9403bc4
andrew <uandrew1965@gmail.com>
2018-06-23 15:42:55 +0300

    .

--- functional-tests.py
+++ functional-tests.py
@@ -9,11 +9,23 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         "Установка"
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Firefox()
 
     def tearDown(self):
         "Демонтаж"
         self.browser.quit()
+
+    def imputToDo(self, toDo):
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+        inputbox.send_keys(toDo)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+    def checkRowInToDoTabel(self, rowText):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(rowText, [row.text for row in rows])
 
     def test_попробуем_статртануть_и_доделаем_позже(self):
         self.browser.get("http://localhost:8000")
@@ -23,18 +35,11 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.imputToDo("Купить павлиньи перья")
+        self.imputToDo("Сделать мушку из павлиньих перев")
 
-        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
-
-        inputbox.send_keys("Купить павлиньи перья")
-        inputbox.send_keys(Keys.ENTER)
-
-        time.sleep(1)
-
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1: Купить павлиньи перья" for row in rows), "Новый элемент списка не появился в таблице.")
+        self.checkRowInToDoTabel("1: Купить павлиньи перья")
+        self.checkRowInToDoTabel("2: Сделать мушку из павлиньих перев")
 
         self.fail("Закончить тест ...")
 
