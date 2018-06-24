68d1e6d7a26787e17add3a131d4db7e86ef3024d
andrew <uandrew1965@gmail.com>
2018-06-24 19:25:23 +0300

    .

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,7 +12,8 @@ class NewVisitorTest(LiveServerTestCase):
         "Установка"
         self.toDoList = [
             "Купить павлиньи перья",
-            "Сделать мушку из павлиньих перев"
+            "Сделать мушку из павлиньих перев",
+            "Купить молоко"
         ]
         self.browser = webdriver.Firefox()
         self.MAX_WAIT = 10
@@ -43,9 +44,7 @@ class NewVisitorTest(LiveServerTestCase):
                 return
 
     def test_can_start_a_list_for_one_user(self):
-        """
 
-        """
         self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
@@ -59,4 +58,36 @@ class NewVisitorTest(LiveServerTestCase):
         self.imputToDo(self.toDoList[1])
         self.checkRowInToDoTabel(f"2: {self.toDoList[1]}")
 
-        self.fail("Закончить тест ...")
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn('To-Do', header_text)
+
+        self.imputToDo(self.toDoList[0])
+        self.checkRowInToDoTabel(f"1: {self.toDoList[0]}")
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+
+        page_text = self.browser.find_elements_by_tag_name("body").text
+        self.assertNotIn( self.toDoList[0], page_text)
+        self.assertNotIn( self.toDoList[1], page_text)
+
+        self.imputToDo(self.toDoList[3])
+        self.checkRowInToDoTabel(f"1: {self.toDoList[3]}")
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_elements_by_tag_name("body").text
+        self.assertNotIn(self.toDoList[0], page_text)
+        self.assertIn(self.toDoList[2], page_text)
