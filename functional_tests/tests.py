b5d685094e99b43c5b8964d36a655a6122dfe6d6
andrew <uandrew1965@gmail.com>
2018-06-25 20:15:33 +0300

    .

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,7 +16,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
             "Купить молоко"
         ]
         self.browser = webdriver.Firefox()
-        self.MAX_WAIT = 2
+        self.MAX_WAIT = 10
         self.MIN_WAIT = 0.1
 
     def tearDown(self):
