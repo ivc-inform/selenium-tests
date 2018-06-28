2342b15d76625214b6aba8339a06c6b38822f319
andrew <uandrew1965@gmail.com>
2018-06-28 17:48:56 +0300

    Перемещенная база

--- deoloy-tools/fabfile.py
+++ deoloy-tools/fabfile.py
@@ -1,35 +1,58 @@
+import random
+
 from fabric.api import env
-from fabric.contrib.files import exists
+from fabric.contrib.files import exists, sed, append
 from fabric.operations import run, local, sudo
 
 REPO_URL = "https://github.com/ivc-inform/selenium-tests.git"
 
 
-# fab -u uandrew -p dfqc2  --sudo-password=dfqc2 deploy:host=192.168.0.104
+# fab -u uandrew -p dfqc2 --sudo-password=dfqc2 deploy:host=192.168.0.104
 
 def deploy():
-    site_folder = f"/home/{env.user}/nginx/sites/{env.host}"
-    source_folder = f"{site_folder}/source"
+    siteName = env.host
+    siteFolder = f"/home/{env.user}/nginx/sites/{siteName}"
+    sourceFolder = f"{siteFolder}/source"
 
-    sudo("apt install apache2-utils")
-    sudo("add-apt-repository --yes ppa:fkrull/deadsnakes")
-    sudo("apt update")
-    sudo("apt install nginx git python3.6 python3.6-venv git")
+    # sudo("apt install apache2-utils")
+    # sudo("add-apt-repository --yes ppa:fkrull/deadsnakes")
+    # sudo("apt update")
+    # sudo("apt install nginx git python3.6 python3.6-venv git")
+    #
+    # createDirectoryStructure(siteFolder)
+    # getSources(sourceFolder)
+    updateSetting(sourceFolder, siteName)
 
-    createDirectoryStructure(site_folder)
-    getSources(source_folder)
 
-
-def createDirectoryStructure(site_folder):
+def createDirectoryStructure(siteFolder):
     for subfolder in ("database", "source", "static", "virtualenv"):
-        run(f"mkdir -p {site_folder}/{subfolder}")
+        run(f"mkdir -p {siteFolder}/{subfolder}")
 
 
-def getSources(source_folder):
-    if exists(f"{source_folder}/.git"):
-        run(f"cd {source_folder} && git fetch")
+def getSources(sourceFolder):
+    if exists(f"{sourceFolder}/.git"):
+        run(f"cd {sourceFolder} && git fetch")
     else:
-        run(f"git clone {REPO_URL} {source_folder}")
+        run(f"git clone {REPO_URL} {sourceFolder}")
 
-    current_commit = local("git log -n 1 --format=%H" , capture=True)
-    run(f"cd {source_folder} && git reset --hard {current_commit}")
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f"cd {sourceFolder} && git reset --hard {current_commit}")
+
+
+def updateSetting(sourceFolder, siteName):
+    settingPath = f"{sourceFolder}/project/settings.py"
+    sed(settingPath, "DEBUG=True", "DEBUG=False")
+    sed(settingPath, "ALLOWED_HOSTS=.+$", f"ALLOWED_HOSTS=[{siteName}]")
+
+    secretKeyFile = f"{sourceFolder}/project/secret_key.py"
+    if not exists(secretKeyFile):
+        chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+        key = "".join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secretKeyFile, f"SECRET_KEY={key}")
+    append(settingPath, "\nfrom .secret_key import SECRET_KEY")
+
+
+if __name__ == "__main__":
+    chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+    key = "".join(random.SystemRandom().choice(chars) for _ in range(50))
+    print(key)
