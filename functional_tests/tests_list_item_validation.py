80af06f3879f772ea57076811f30e5ae8c1f9f22
andrew <uandrew1965@gmail.com>
2018-06-29 19:35:50 +0300

    Перемещенная база

--- functional_tests/tests_list_item_validation.py
+++ functional_tests/tests_list_item_validation.py
@@ -11,4 +11,18 @@ class ItemValidationTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
         self.wait_forl(lambda: self.assertEqual(self.browser.find_element_by_css_selector(".has_error").text, "You can`t have en empty list item."))
-        self.fail("write me !!")
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_forl(lambda: self.assertEqual(self.browser.find_element_by_css_selector(".has_error").text, "You can`t have en empty list item."))
+
+        inputbox.send_keys("Make tea")
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table("1: Buy milk")
+        self.wait_for_row_in_list_table("2: Make tea")
