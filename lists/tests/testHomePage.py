d8cffac9ab76d08d852971e98e2dffb5f9fc6d92
andrew <uandrew1965@gmail.com>
2018-06-29 20:35:11 +0300

    Перемещенная база

---
+++ lists/tests/testHomePage.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from django.urls import resolve
+
+from lists.settings import templateHomePage
+from lists.views import home_page
+
+
+class TestHomePage(TestCase):
+    def test_root_url(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, templateHomePage)
--- lists/tests/tests_all.py
+++ lists/tests/tests_all.py
@@ -1,19 +1,7 @@
 from django.test import TestCase
-from django.urls import resolve
 
 from lists.models import Item, List
-from lists.settings import templateListPage, templateHomePage, listUrl, message1
-from lists.views import home_page
-
-
-class HomePageTest(TestCase):
-    def test_root_url(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, templateHomePage)
+from lists.settings import templateListPage, listUrl, message1
 
 
 class ItemModelTest(TestCase):
