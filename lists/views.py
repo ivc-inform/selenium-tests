3ef990eed33b3d1ef5934aff3ed15f4a308d15e6
andrew <uandrew1965@gmail.com>
2018-07-02 23:21:09 +0300

    Перемещенная база

--- lists/views.py
+++ lists/views.py
@@ -13,18 +13,19 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    form_ = ItemForm()
 
     if request.method == "POST":
         Item.objects.create(text=request.POST["text"], list=list_)
         return redirect(f"/lists/{list_.id}/")
-    return render(request, templateListPage, dict(list=list_))
+    return render(request, templateListPage, dict(list=list_, form=form_))
 
 
 def new_list(request):
-    form = ItemForm(data=request.POST)
-    if form.is_valid():
+    form_ = ItemForm(data=request.POST)
+    if form_.is_valid():
         list_ = List.objects.create()
         item = Item.objects.create(text=request.POST["text"], list=list_)
         return redirect(list_)
     else:
-        return render(request, "home.html", dict(form=form))
+        return render(request, "home.html", dict(form=form_))
