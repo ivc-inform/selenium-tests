3db51ada08aae67d1580e4cdb8e8f490902f2593
andrew <uandrew1965@gmail.com>
2018-07-03 08:45:38 +0300

    Перемещенная база

--- functional_tests/tests_list_item_validation.py
+++ functional_tests/tests_list_item_validation.py
@@ -6,22 +6,26 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
-    def test_cannot_add_empty(self):
+    def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_forl(lambda: self.assertEqual(self.browser.find_element_by_css_selector(".has_error").text, EMPTY_ITEM_ERROR))
 
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys("Buy milk")
-        inputbox.send_keys(Keys.ENTER)
+        self.wait_forl(lambda: self.browser.find_element_by_css_selector("#id_text:invalid"))
 
+        self.get_item_input_box().send_keys("Buy milk")
+        self.wait_forl(lambda: self.browser.find_element_by_css_selector("#id_text:valid"))
+
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy milk")
 
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_forl(lambda: self.assertEqual(self.browser.find_element_by_css_selector(".has_error").text, EMPTY_ITEM_ERROR))
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+        self.wait_forl(lambda: self.browser.find_element_by_css_selector("#id_text:invalid"))
 
-        inputbox.send_keys("Make tea")
-        inputbox.send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys("Make tea")
+        self.wait_forl(lambda: self.browser.find_element_by_css_selector("#id_text:valid"))
+
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table("1: Buy milk")
         self.wait_for_row_in_list_table("2: Make tea")
