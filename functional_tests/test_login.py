86911f68d8c5cbf84fca69becab9269763254e33
andrew <uandrew1965@gmail.com>
2018-07-04 01:35:38 +0300

    .

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+
 from functional_tests.base import FunctionalTest
 
 TEST_EMAIL = 'test@ivc-info.ru'
@@ -5,4 +7,7 @@ SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTets(FunctionalTest):
-    ...
+    def test_can_get_email_link_to_log_in(self):
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
