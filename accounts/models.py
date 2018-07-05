6c82afbacf6d9ca3f893eab7d7ef0ca62f8e7896
andrew <uandrew1965@gmail.com>
2018-07-05 04:04:08 +0300

    .

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
-from django.db.models import Model, EmailField
+from django.db.models import Model, EmailField, CharField
 
 
 class User(Model):
@@ -11,3 +13,8 @@ class User(Model):
     USERNAME_FIELD = 'email'
     is_anonymous = False
     is_authenticated = True
+
+
+class Token(Model):
+    email = EmailField()
+    uid = CharField(max_length=40, default=uuid.uuid4)
