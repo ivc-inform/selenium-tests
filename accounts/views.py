fe5769a83d11532a5c1f37d8e78c4dee8ba7275e
andrew <uandrew1965@gmail.com>
2018-07-05 14:46:39 +0300

    .

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def send_login_email(request):
+    email = request.POST["email"]
+    send_mail(
+        'Your login link for Superlists',
+        'body',
+        'noreplay@superlists',
+        [email]
+    )
     return redirect("/")
