dc99a36ec29f3ca238748bde302108a5ed9a890b
andrew <uandrew1965@gmail.com>
2018-07-05 22:10:37 +0300

    .

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,11 @@ from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+
 # Create your views here.
 def send_login_email(request):
     email = request.POST["email"]
-    print(type(send_mail))
+    # print(type(send_mail))
     send_mail(
         'You login link for List To-Do',
         'Use this link to log in',
@@ -13,4 +14,9 @@ def send_login_email(request):
         [email]
     )
     messages.success(request, "Проверьте свою почту, мы отправили Вам ссылку, которую можно использовать для входа на сайт")
+    return redirect("/")
+
+
+def login(request):
+    print(f"token: {request.GET.get('token')}")
     return redirect("/")
