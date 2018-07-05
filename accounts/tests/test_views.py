86f708ad467ecfd7042a0eea8c326fd93827b07e
andrew <uandrew1965@gmail.com>
2018-07-05 16:34:39 +0300

    .

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 import accounts.views
@@ -7,21 +9,22 @@ class SendLoginEmailViewTest(TestCase):
     def test_redirects_to_home_page(self):
         response = self.client.post("/accounts/send_login_email/", data=dict(email="test@ivc-inform.ru"))
         self.assertRedirects(response, "/")
+    @patch('accounts.views.send_mail')
+    def test_send_mail_to_address_from_post(self, mock_send_mail=None):
+        # self.send_mail_called = False
 
-    def test_send_mail_to_address_from_post(self):
-        self.send_mail_called = False
-
-        def fake_send_mail(subject, body, from_email, to_list):
-            self.send_mail_called = True
-            self.subject = subject
-            self.body = body
-            self.from_email = from_email
-            self.to_list = to_list
-
-        accounts.views.send_mail = fake_send_mail
+        # def fake_send_mail(subject, body, from_email, to_list):
+        #     self.send_mail_called = True
+        #     self.subject = subject
+        #     self.body = body
+        #     self.from_email = from_email
+        #     self.to_list = to_list
+        #
+        # accounts.views.send_mail = fake_send_mail
 
         self.client.post("/accounts/send_login_email/", data=dict(email="edith@example.com"))
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, "Your login link for Superlists")
-        self.assertEqual(self.from_email, "noreplay@superlists")
-        self.assertEqual(self.to_list, ["edith@example.com"])
+        self.assertEqual(mock_send_mail.called, True)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, "Your login link for Superlists")
+        self.assertEqual(from_email, "noreplay@superlists")
+        self.assertEqual(to_list, ["edith@example.com"])
