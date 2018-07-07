e2edeb3eeb7b5f96c10f9b029cc1bf6d700dbb38
andrew <uandrew1965@gmail.com>
2018-07-07 11:54:04 +0300

    .

---
+++ functional_tests/test_my_lists.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model, SESSION_KEY, BACKEND_SESSION_KEY
+from django.contrib.sessions.backends.db import SessionStore
+
+from functional_tests.base import FunctionalTest
+from project import settings
+
+User = get_user_model()
+
+
+class MyListsTest(FunctionalTest):
+    def create_autenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        self.browser.get(self.live_server_url + "/404_no_such_url")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path="/"
+        ))
