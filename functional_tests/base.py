8846c6dfaef442d2d2b3d94ac6abc5a1252e5c0e
andrew <uandrew1965@gmail.com>
2018-07-07 18:09:43 +0300

    .

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -80,7 +80,7 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     @wait
     def wait_to_be_logged_in(self, email):
-        self.browser.find_element_by_link_text("Log out")
+        self.browser.find_element_by_link_text("Log in")
         navbar = self.browser.find_element_by_css_selector(".navbar")
         print(f" navbar.text: { navbar.text}")
         self.assertIn(email, navbar.text)
