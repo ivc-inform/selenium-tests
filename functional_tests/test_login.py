8cc006c89964468eb6799e53124ee0238cd44e58
andrew <uandrew1965@gmail.com>
2018-07-07 12:35:55 +0300

    .

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -3,22 +3,22 @@ import re
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
 
+from accounts.tests.test_views import EMAIL_TEST
 from functional_tests.base import FunctionalTest
 
-TEST_EMAIL = 'test@ivc-info.ru'
 SUBJECT = 'You login link for List To-Do'
 
 
 class LoginTets(FunctionalTest):
     def test_can_get_email_link_to_log_in(self):
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(EMAIL_TEST)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         self.wait_forl(lambda: self.assertIn('Проверьте свою почту, мы отправили Вам ссылку, которую можно использовать для входа на сайт', self.browser.find_element_by_tag_name('body').text))
 
         email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
+        self.assertIn(EMAIL_TEST, email.to)
         self.assertEqual(email.subject, SUBJECT)
 
         self.assertIn("Use this link to log in", email.body)
@@ -32,10 +32,10 @@ class LoginTets(FunctionalTest):
 
         self.wait_forl(lambda: self.browser.find_element_by_link_text("Log out"))
         navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.assertIn(EMAIL_TEST, navbar.text)
 
         self.browser.find_element_by_link_text("Log out").click()
         self.wait_forl(lambda :self.browser.find_element_by_name("email"))
 
         navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.assertNotIn(EMAIL_TEST, navbar.text)
