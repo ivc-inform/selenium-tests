04e8e46f402c684b75b6b5f056fc4f4c76b58138
andrew <uandrew1965@gmail.com>
2018-07-08 11:25:09 +0300

    .

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -23,11 +23,13 @@ class LoginTets(FunctionalTest):
         email_id = None
         start = time.time()
         inbox = poplib.POP3_SSL("mail.hostland.ru")
+        print(inbox.getwelcome())
         try:
             inbox.user(test_email)
             inbox.pass_(os.environ['TEST_BOX_PASS'])
             while time.time() - start < 60:
                 count, _ = inbox.stat()
+                print(f"count:{count}")
                 for i in reversed(range(max(1, count - 10), count + 1)):
                     print('getting msg', i)
                     _, lines, __ = inbox.retr(i)
@@ -38,6 +40,7 @@ class LoginTets(FunctionalTest):
                         body = "\n".join(lines)
                         return body
                 time.sleep(5)
+
         finally:
             if email_id:
                 inbox.dele(email_id)
@@ -50,19 +53,37 @@ class LoginTets(FunctionalTest):
 
         self.wait_for(lambda: self.assertIn('Проверьте свою почту, мы отправили Вам ссылку, которую можно использовать для входа на сайт', self.browser.find_element_by_tag_name('body').text))
 
+        print("point 1")
         body = self.wait_for_email(EMAIL_TEST, SUBJECT)
+        print("point 2")
 
         self.assertIn("Use this link to log in", body)
+        print("point 3")
         url_search = re.search(r"http://.+/.+$", body)
+        print("point 4")
 
         if not url_search:
             self.fail(f"Could not find url in email body:\n{body}")
 
         url = url_search.group(0)
+        print("point 5")
         self.assertIn(self.live_server_url, url)
+        print("point 6")
 
+        print("url", url)
         self.browser.get(url)
-
+        print("point 7")
         self.wait_to_be_logged_in(email=EMAIL_TEST)
         self.browser.find_element_by_link_text("Log out").click()
         self.wait_to_be_logged_out(email=EMAIL_TEST)
+
+if __name__ == '__main__':
+    import getpass, poplib
+
+    M = poplib.POP3_SSL('mail.hostland.ru')
+    M.user("test@ivc-inform.ru")
+    M.pass_("mt30718tm")
+    numMessages = len(M.list()[1])
+    for i in range(numMessages):
+        for j in M.retr(i + 1)[1]:
+            print(j)
