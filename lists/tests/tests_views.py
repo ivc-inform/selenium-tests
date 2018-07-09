0bd28d7289982eab6f362c7be49b56a776dfa73a
andrew <uandrew1965@gmail.com>
2018-07-09 15:17:16 +0300

    .

--- lists/tests/tests_views.py
+++ lists/tests/tests_views.py
@@ -88,8 +88,7 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-@skip
 class MyListsTest(TestCase):
     def test_my_lists_url_renders_my_lists_template(self):
-        response = self.client.get("/lists/users/a@n.com")
+        response = self.client.get("/lists/users/a@b.com/")
         self.assertTemplateUsed(response, "my_lists.html")
