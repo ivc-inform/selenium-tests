143beaf736220c05fe8710ce1dc758c5a405fc81
andrew <uandrew1965@gmail.com>
2018-07-09 18:51:31 +0300

    .

--- lists/tests/tests_views.py
+++ lists/tests/tests_views.py
@@ -89,6 +89,13 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_list_owner_is_saved_if_user_is_autenticated(self):
+        user = User.objects.create(email="a@b.com")
+        self.client.force_login(user)
+        self.client.post("/lists/new", data=dict(text="new_item"))
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, user)
+
 class MyListsTest(TestCase):
     def test_my_lists_url_renders_my_lists_template(self):
         User.objects.create(email="a@b.com")
